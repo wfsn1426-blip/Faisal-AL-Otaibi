344c66e43360b112a5dc97c718ef51250ad07a7d
wfsn1426-blip <w.f.s.n1426@gmail.com>
2025-12-25 12:02:52 +0300

    Update App.tsx

--- attendance-system/src/app/App.tsx
+++ attendance-system/src/app/App.tsx
@@ -1,1 +1,14 @@
+import { useState } from "react";
+import Splash from "./screens/Splash";
 
+export default function App() {
+  const [screen, setScreen] = useState<"splash" | "login">("splash");
+
+  return (
+    <>
+      {screen === "splash" && (
+        <Splash onFinish={() => setScreen("login")} />
+      )}
+    </>
+  );
+}
