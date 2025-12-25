95ae05f27508625f4489507196cd9cd50f3ec779
wfsn1426-blip <w.f.s.n1426@gmail.com>
2025-12-25 12:05:58 +0300

    Update App.tsx

--- attendance-system/src/app/App.tsx
+++ attendance-system/src/app/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import Splash from "./screens/Splash";
+import Login from "./screens/Login";
 
 export default function App() {
-  const [screen, setScreen] = useState<"splash" | "login">("splash");
+  const [screen, setScreen] = useState<"splash" | "login" | "home">("splash");
 
   return (
     <>
       {screen === "splash" && (
         <Splash onFinish={() => setScreen("login")} />
+      )}
+
+      {screen === "login" && (
+        <Login onLogin={() => setScreen("home")} />
       )}
     </>
   );
